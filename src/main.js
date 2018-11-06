d35ef43fe782a644677e79fe3a1529192ba12ae5
German Varanytsya <s12943@pjwstk.edu.pl>
2018-11-06 15:44:35 +0200

    hide added, components added

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import Card from './components/Card.vue'
 
 Vue.use(ElementUI);
 Vue.use(VueResource);
+Vue.component('app-card', Card);
 
 new Vue({
   el: '#app',
